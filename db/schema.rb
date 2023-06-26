3eca3ab9fc440aca7e4b23c40800106cf00a42b6
marlena-b <marlena.borowiec01@gmail.com>
2023-06-26 12:43:33 +0200

    Add products CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_24_165111) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_26_091753) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "sku"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
