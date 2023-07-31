0df17fb0a0c8186cb36cb1014bb6f83dadc513b1
marlena-b <marlena.borowiec01@gmail.com>
2023-07-31 15:10:30 +0200

    Refactor total quantity calculations

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 module ProductsHelper
-  def total_quantity_of_products(product)
-    total_quantity = []
-    product.stocks.each do |stock|
-      total_quantity << stock.quantity
-    end
-    total_quantity.sum
-  end
 end
